4538aaed30114875a193ec9c764f75ba197ba302
ngel rojas <aomarrojasp@gmail.com>
2023-07-06 21:11:47 -0300

    add: remove test, and remove all npm help to run jenkins without problems

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -8,8 +8,4 @@ test('home', () => {
   expect(
     main.getByRole('heading', { level: 1, name: /welcome to next\.js!/i })
   ).toBeDefined()
-
-  // const footer = within(screen.getByRole('contentinfo'))
-  // const link = within(footer.getByRole('link'))
-  // expect(link.getByRole('img', { name: /vercel logo/i })).toBeDefined()
 })
